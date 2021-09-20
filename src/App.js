dc494edc5b59573a502fa21e993152afb8cc0539
sieunlee <dltldmsm@gmail.com>
2021-09-20 14:28:48 +0900

    210920

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="header">
+        <h1>COVID-19 DATA</h1>
+        <select>
+          <option>국내</option>
+          <option>세계</option>
+        </select>
+      </header>
     </div>
   );
 }
