513f194a763ae9c9064d676349dea7de1440e331
sieunlee <dltldmsm@gmail.com>
2021-09-20 17:24:03 +0900

    210920

--- src/component/Conents.js
+++ src/component/Conents.js
@@ -4,17 +4,7 @@ import axios from 'axios'
 
 export default function Conents() {
 
-    const  [confirmedData, setConfirmedData] = useState({
-        labels: ["1월", "2월", "3월"],
-        datasets: [
-            {
-                label: "국내 누적 확진자",
-                backgroundColor: "salmon",
-                fill: true,
-                data:[10,5,3]
-            },
-        ]
-    });
+    const  [confirmedData, setConfirmedData] = useState();
     
     const options={ 
         title:{ display: true, text: "누적 확진자 추이", fontSize: 16 },
@@ -34,10 +24,47 @@ export default function Conents() {
                 const year = curruntDate.getFullYear();
                 const month = curruntDate.getMonth();
                 const date = curruntDate.getDate();
+                const confirmed = cur.Confirmed;
+                const active = cur.Active;
+                const deaths = cur.Deaths;
+                const recovered = cur.Recovered;
 
-                console.log(year, month, date);
+                const findItem = acc.find(a=> a.year === year && a.month === month);
+                
+                if(!findItem){
+                    acc.push({
+                        year, month, date, confirmed, active, deaths, recovered
+                    })
+                }
+
+                if(findItem && findItem.data < items.data){
+                    findItem.year = year;
+                    findItem.monthr = month;
+                    findItem.date = date;
+                    findItem.date = date;
+                    findItem.confirmed = confirmed;
+                    findItem.active = active;
+                    findItem.deaths = deaths;
+                    findItem.recovered = recovered
+                }
+
+    
                 return acc;
             }, []);
+
+            const labels = arr.map(a=>`${a.month+1}월`)
+            setConfirmedData({
+                labels,
+                datasets: [
+                    {
+                        labrl: "국내 누적 확진자",
+                        backgroundColor: "salmon",
+                        fill: "true",
+                        data: arr.map(a=>a.Confirmed)
+                    },
+                ]
+            });
+
         }
 
         fetchEvent();
