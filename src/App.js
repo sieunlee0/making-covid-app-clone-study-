248e28cbfcdc204ab1f56017215a4bf1b71a3daa
sieunlee <dltldmsm@gmail.com>
2021-09-20 21:22:36 +0900

    210920

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Conentst from './component/Conents'
 function App() {
   return (
     <div className="App">
+      <h2>국내 코로나 현황</h2>
       <Header />
       <Conentst />
 
--- src/component/Conents.js
+++ src/component/Conents.js
@@ -106,10 +106,9 @@ export default function Conents() {
         fetchEvent();
     }, []);
 
-    
+
     return (
         <section>
-            <h2>국내 코로나 현황</h2>
             <div className="contents">
                 <div>
                     <Bar data={ confirmedData } options={ options } />
