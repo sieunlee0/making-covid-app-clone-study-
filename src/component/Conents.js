a4bc2192972e4112540cbe16d4c9f2cfadf7a652
sieunlee <dltldmsm@gmail.com>
2021-09-20 16:29:05 +0900

    210920

--- src/component/Conents.js
+++ src/component/Conents.js
@@ -4,7 +4,22 @@ import axios from 'axios'
 
 export default function Conents() {
 
-    const  [confirmedData, setConfirmedData] = useState({});
+    const  [confirmedData, setConfirmedData] = useState({
+        labels: ["1월", "2월", "3월"],
+        datasets: [
+            {
+                label: "국내 누적 확진자",
+                backgroundColor: "salmon",
+                fill: true,
+                data:[10,5,3]
+            },
+        ]
+    });
+    
+    const options={ 
+        title:{ display: true, text: "누적 확진자 추이", fontSize: 16 },
+        legend:{display: true, positon: "bottom"},
+    };
 
     useEffect(()=> {
 
@@ -13,17 +28,14 @@ export default function Conents() {
         }
         fetchEvent();
 
-    })
+    });
 
     return (
         <section>
             <h2>국내 코로나 현황</h2>
             <div className="contents">
                 <div>
-                    <Bar data={ confirmedData } options={ 
-                        {title:{ display: true, text: "누적 확진자 추이", fontSize: 16 }},
-                        {legend:{display: true, positon: "bottom"}}
-                    } />
+                    <Bar data={ confirmedData } options={options} />
                 </div>
             </div>
         </section>
