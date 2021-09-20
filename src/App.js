c47851b473530811fa10ede0447da6b1e15fb52f
sieunlee <dltldmsm@gmail.com>
2021-09-20 14:36:58 +0900

    210920

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import Header from './component/Header'
+import Conentst from './component/Conents'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Conentst />
 
-      <section>
-        <h2>국내 코로나 현황</h2>
-        <div className="contents">
-          
-        </div>
-      </section>
     </div>
   );
 }
---
+++ src/component/Conents.js
@@ -0,0 +1,12 @@
+
+
+export default function Conents() {
+    return (
+        <section>
+        <h2>국내 코로나 현황</h2>
+        <div className="contents">
+
+        </div>
+      </section>
+    );
+}
