2d5c336cf69c01e760563afaccb57df4f04cd8ec
sieunlee <dltldmsm@gmail.com>
2021-09-20 17:30:03 +0900

    210920

--- src/component/Conents.js
+++ src/component/Conents.js
@@ -57,7 +57,7 @@ export default function Conents() {
                 labels,
                 datasets: [
                     {
-                        labrl: "국내 누적 확진자",
+                        label: "국내 누적 확진자",
                         backgroundColor: "salmon",
                         fill: "true",
                         data: arr.map(a=>a.Confirmed)
