825a49e5b0c18755f37bd41e468f428e0d17a244
sieunlee <dltldmsm@gmail.com>
2021-09-20 15:39:07 +0900

    210920

--- src/component/Conents.js
+++ src/component/Conents.js
@@ -1,12 +1,30 @@
-
+import { useState, useEffect } from "react";
+import { Bar, Doughnut, Line} from "react-chartjs-2";
+import axios from 'axios' 
+import { getByDisplayValue } from "@testing-library/react";
 
 export default function Conents() {
+
+    useEffect(()=> {
+
+        const fetchEvent = async ()=>{
+            const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr");
+        }
+        fetchEvent();
+
+    })
+
     return (
         <section>
-        <h2>국내 코로나 현황</h2>
-        <div className="contents">
-
-        </div>
-      </section>
+            <h2>국내 코로나 현황</h2>
+            <div className="contents">
+                <div>
+                    <Bar data={ confirmedData } options={ 
+                        {title:{ display: true, text: "누적 확진자 추이", fontSize: 16 }},
+                        {legend:{display: true, positon: "bottom"}}
+                    }
+                </div>
+            </div>
+        </section>
     );
 }
