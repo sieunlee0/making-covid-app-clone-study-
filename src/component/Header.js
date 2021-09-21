95d881c086274a1b222b89735bf272637be05e41
sieunlee <dltldmsm@gmail.com>
2021-09-21 19:32:31 +0900

    210921

--- src/component/Header.js
+++ src/component/Header.js
@@ -16,8 +16,8 @@ function Header() {
         <header className="header">
             <h1>COVID-19 DATA</h1>
             <select>
-            <option>국내</option>
-            <option>세계</option>
+                <option>국내</option>
+                <option>세계</option>
             </select>
         </header>
     );
