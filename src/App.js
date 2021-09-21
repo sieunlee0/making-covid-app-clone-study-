a55f22a8ac57c0ba595c47020f1ebcd3e56b79e8
sieunlee <dltldmsm@gmail.com>
2021-09-21 19:16:56 +0900

    210921

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Header from './component/Header'
-import Conentst from './component/Conents'
+import Header from './component/Header';
+import Contents from './component/Contents';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <h2 className="title">국내 코로나 현황</h2>
-      <Conentst />
+      <Contents />
 
     </div>
   );
