62602e9dd6099d72b4ec04ab83e6e66c3a677090
sieunlee <dltldmsm@gmail.com>
2021-09-24 22:14:52 +0900

    210924

--- src/component/Header.js
+++ src/component/Header.js
@@ -25,9 +25,10 @@ function Header() {
 
 export default Header;
 
+// 210923 공부 내용
 // <select>는 옵션 메뉴를 제공하는 컨트롤러를 나타내주는 태그/ <option>과 함께 쓰인다.
 // <option>은 <select>, <optgroup>, <datalist>의 항목을 정할 때 쓰인다.
 // <optgroup>은 <select>의 옴션들을 묶을 수 있다.
-// 
+// <datalist>는 옵션을 따로 입력 가능한 인풋 컨트롤러를 나타내준다. 옵션은 추천 선택지이며, 그중에 선택도 가능하다.
 
 
