30f16a3052e2bb49dbdbbcb411234ddf67b05a72
sieunlee <dltldmsm@gmail.com>
2021-09-24 22:12:18 +0900

    210924

--- src/component/Header.js
+++ src/component/Header.js
@@ -27,6 +27,7 @@ export default Header;
 
 // <select>는 옵션 메뉴를 제공하는 컨트롤러를 나타내주는 태그/ <option>과 함께 쓰인다.
 // <option>은 <select>, <optgroup>, <datalist>의 항목을 정할 때 쓰인다.
+// <optgroup>은 <select>의 옴션들을 묶을 수 있다.
 // 
 
 
