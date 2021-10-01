cd5810fc94831d7db812350f5d2a769fb9caa584
sieunlee <dltldmsm@gmail.com>
2021-10-01 20:49:23 +0900

    211001

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h2 className="title">국내 코로나 현황</h2>
+        <h2 className="title">국내 코로나 현황</h2>
       <Contents />
-
     </div>
   );
 }
